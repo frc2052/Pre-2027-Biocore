c1319654d840cbd7bd2d234720ab68f609052342
Maxwell Swanson <nosnaws1019@gmail.com>
2026-09-25 17:32:59 -0500

    updated the applyAllMeasurements() method to take into account the predicted pose with treating jerk as constant and also ran a build for formatting.

--- src/main/java/com/team2052/lib/geometry/ChassisJerks.java
+++ src/main/java/com/team2052/lib/geometry/ChassisJerks.java
@@ -2,50 +2,50 @@ package com.team2052.lib.geometry;
 
 public class ChassisJerks {
 
-    /** meters / seconds ^ 3 */
-    public double jx;
+  /** meters / seconds ^ 3 */
+  public double jx;
 
-    /** meters / seconds ^ 3 */
-    public double jy;
+  /** meters / seconds ^ 3 */
+  public double jy;
 
-    /** radians / seconds ^ 3 */
-    public double zeta;
-    
-    public ChassisJerks(double jx, double jy,  double zeta) {
-        this.jx = jx;
-        this.jy = jy;
-        this.zeta = zeta;
-    }
+  /** radians / seconds ^ 3 */
+  public double zeta;
 
-    public ChassisJerks() {
-        jx = 0;
-        jy = 0;
-        zeta = 0;
-    }
+  public ChassisJerks(double jx, double jy, double zeta) {
+    this.jx = jx;
+    this.jy = jy;
+    this.zeta = zeta;
+  }
 
-    public ChassisJerks plus(ChassisJerks jerk) {
-        return new ChassisJerks(jx+jerk.jx, jy+jerk.jy, zeta+jerk.zeta);
-    }
+  public ChassisJerks() {
+    jx = 0;
+    jy = 0;
+    zeta = 0;
+  }
 
-    public ChassisJerks minus(ChassisJerks jerk) {
-        return plus(jerk.unaryMinus());
-    }
+  public ChassisJerks plus(ChassisJerks jerk) {
+    return new ChassisJerks(jx + jerk.jx, jy + jerk.jy, zeta + jerk.zeta);
+  }
 
-    public ChassisJerks unaryMinus() {
-        return new ChassisJerks(-jx, -jy, -zeta);
-    }
+  public ChassisJerks minus(ChassisJerks jerk) {
+    return plus(jerk.unaryMinus());
+  }
 
-    public ChassisJerks times(double scalar) {
-        return new ChassisJerks(jx*scalar, jy*scalar, zeta*scalar);
-    }
+  public ChassisJerks unaryMinus() {
+    return new ChassisJerks(-jx, -jy, -zeta);
+  }
 
-    public ChassisJerks div(double divisor) {
-        return times(1/divisor);
-    }
+  public ChassisJerks times(double scalar) {
+    return new ChassisJerks(jx * scalar, jy * scalar, zeta * scalar);
+  }
 
-    @Override
-    public String toString() {
-        return String.format(
+  public ChassisJerks div(double divisor) {
+    return times(1 / divisor);
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
         "ChassisJerks(Jx: %.2f m/s³, Jy: %.2f m/s³, Zeta: %.2f rad/s³)", jx, jy, zeta);
-    }
+  }
 }
